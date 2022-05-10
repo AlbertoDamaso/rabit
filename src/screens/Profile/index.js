aaf1b7591f25313f5b1fdc18b5489abe1f8b08cf
AlbertoDamaso <alberto.matheus@hotmail.com>
2022-05-10 17:00:58 -0300

    profile

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,15 +1,67 @@
 import React from 'react';
-
 import {
-  View
+  View,
+  Text
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+import { Background } from '../../components/Background';
+import { BtnDrawer } from '../../components/BtnDrawer';
+import { ImgProfile } from '../../components/ImgProfile';
+import { Button } from '../../components/Button';
+import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
 export function Profile() {
-  return (
-    <View>
+  const navigation = useNavigation();
 
-    </View>
+  function handleHome(){
+    navigation.navigate('Reservar');
+  }
+
+  return (
+    <Background>
+      <View style={styles.container}>
+
+        <BtnDrawer/>
+
+        <View style={styles.areaProfile}>
+          <ImgProfile
+            style={styles.imageProfile}
+          /> 
+          <Text style={styles.nameProfile}>
+            Alberto Matheus  
+          </Text>   
+          <Text style={styles.revProfile}>
+            20 reservas
+          </Text>
+          <Text style={[styles.revProfile,{marginTop: 7}]}>
+            12 retiradas
+          </Text>
+        </View>
+
+        <View style={styles.areBtn}>
+          <View style={styles.formatBtn}>
+            <Button
+              onPress={handleHome}
+              title={'Reservar mais'}
+              activeOpacity={0.7}
+            />
+          </View> 
+          <View style={styles.formatBtn}>
+            <Button
+              title={'Retirar/Contato'}
+              activeOpacity={0.7}
+            />  
+          </View>
+
+          <Button
+            title={'Sair'}
+            activeOpacity={0.7}
+            style={styles.btnOut}
+          />                  
+        </View>
+      </View>
+    </Background>
   );
 }
--- src/screens/Profile/styles.js
+++ src/screens/Profile/styles.js
@@ -1,7 +1,53 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  areaProfile: {
+    width: '80%',
+    height: 220,
+    marginTop: 140,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  imageProfile: {
+    width: 109,
+    height: 109,
+    borderRadius: 90,
+    borderWidth:1,
+    borderColor: theme.colors.primary,
+  },
+  nameProfile: {
+    fontFamily: theme.fonts.title,
+    color: theme.colors.light,
+    fontSize: 19,
+    marginTop: 21,
+  },
+  revProfile: {
+    fontFamily: theme.fonts.text,
+    color: theme.colors.primaryMenos,
+    fontSize: 18,
+    marginTop: 21,
+  },
+  areBtn: {
+    marginTop: 47,
+  },
+  formatBtn: {
+    marginTop: 18,
+  },
+  btnOut: {
+    width: '60%',
+    height: 45,
+    marginTop: 28,
+    borderWidth:1,    
+    alignSelf:'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    borderRadius: 25,
+    borderColor: theme.colors.primaryMais,
+    backgroundColor: theme.colors.primaryMais,
   }
 });
